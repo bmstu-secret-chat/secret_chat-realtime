d24abfa521580d3e2e7bea67d1c7e801d0d7d128
arklem <jrsy.tdh@yandex.ru>
2025-01-11 15:11:18 +0300

    add url

--- realtime/urls.py
+++ realtime/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/message/', include('message.urls')),
+    path('api/realtime/', include('realtime.routing')),
 ]
